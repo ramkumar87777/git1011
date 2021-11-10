237cb0a1450589ab92d0339da2f83a79662ca5f9
ramkumar87777 <ramsuryasankararudv@gmail.com>
2021-11-10 11:33:17 +0530

    Ramkumar

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,22 @@
+package org.add;
+
+public class GreensTech {
+public void greensOmr() {
+	System.out.println("OMR");
+}
+public void greensAdayar() {
+System.out.println("Adayar");
+}
+public void greensTambaram() {
+System.out.println("Tambaram");
+}
+
+public static void main(String[] args) {
+	GreensTech GT = new GreensTech(); 
+	
+	GT.greensAdayar();
+	GT.greensAdayar();
+	GT.greensOmr();
+	GT.greensTambaram();
+}
+}
